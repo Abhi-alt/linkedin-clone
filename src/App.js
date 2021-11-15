6bcd0c7d9fa260693f19c1a72bf9d5bccaf0a0c8
Abhishek Kumar <chocolate.ladu@gmail.com>
2021-11-15 10:56:46 +0530

    Login component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <h2>LINKEDIN CLONE</h2>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
