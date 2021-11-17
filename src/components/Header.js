45fadedd919bee6d8ce0ba6c8fad288afaaf5d83
Abhishek Kumar <chocolate.ladu@gmail.com>
2021-11-17 10:56:42 +0530

    Nav menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,22 @@ const Header = (props) =>{
     return(
         <Container>
             <Content>
-                Header
+                <Logo>
+                    <a href="/home">
+                        <img src="/images/home-logo.svg" alt="" />
+                    </a>
+                </Logo>
+                <Search>
+                    <div>
+                        <input type="text" placeholder="Search" />
+                    </div>
+                    <SearchIcon>
+                        <img src="/images/search-icon.svg" />
+                    </SearchIcon>
+                </Search>
+                <Nav>
+                    Nav menu
+                </Nav>
             </Content>
         </Container>
     );
@@ -28,4 +43,55 @@ const Content = styled.div`
     margin: 0 auto;
     min-height: 100%;
     max-width: 1128px;
+`;
+const Logo = styled.span`
+    margin-right: 8px;
+    font-size: 0px;
+`;
+const Search = styled.div`
+    opacity: 1;
+    flex-grow: 1;
+    position: relative;
+    & > div{
+        max-width: 200px;
+        input{
+            border: none;
+            box-shadow: none;
+            background-color: #eef3f8;
+            border-radius: 2px;
+            color: rgba(0, 0, 0, 0.9);
+            width: 218px;
+            padding: 0 8px 0 40px;
+            line-height: 1.75;
+            font-weight: 400;
+            font-size: 14px;
+            height: 34px;
+            border-color: #dce6f1;
+            vertical-align: text-top;
+        }
+    }
+`;
+const SearchIcon = styled.div`
+    width: 40px;
+    position: absolute;
+    z-index: 1;
+    top: 10px;
+    left: 2px;
+    border-radius: 0 2px 2px 0;
+    margin: 0;
+    pointer-events: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+const Nav = styled.nav`
+    margin-left: auto;
+    display: block;
+    @media (max-width: 768px){
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        background: white;
+        width: 100%;
+    }
 `;
